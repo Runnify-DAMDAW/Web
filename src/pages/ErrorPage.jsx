9b1c4f885e1de05976255d67a696b897b26d8da5
daviiidleon <daviiidleongutierrez@gmail.com>
2025-03-04 19:30:34 +0100

    nuevo commit

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 const ErrorPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-gray-800 text-2xl font-bold">
-      <p>⚠️ Error: Página no encontrada</p>
+      <img src="error.png" alt="Error" className="w-64 h-auto mb-4" />
+      <p>Error: Página no encontrada</p>
       <Link to="/" className="mt-4 px-4 py-2 bg-gray-800 text-white rounded-lg shadow hover:bg-gray-900 transition">
         Volver a Inicio
       </Link>
