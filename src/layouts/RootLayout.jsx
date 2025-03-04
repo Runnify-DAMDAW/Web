5a4fe3e45ccea24695a12bda16d675925bf64347
RociooGaviraa <rociogaviralucena@gmail.com>
2025-03-04 20:25:28 +0100

    sin errores

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 const RootLayout = () => {
   return (
-    <div className='bg-[#909590]'>
-      <header className='sticky top-0 w-full h-28 flex items-center bg-gradient-to-r from-[#2c302e] to-[#537a5a]'>
-          <h1 className='text-white flex px-12 text-4xl font-bold'>Runnify</h1>
+    <div className="bg-[#909590]">
+      <header className="sticky top-0 w-full h-36 px-16 flex items-center bg-gradient-to-r from-[#537a5a] to-[#2c302e]">
+        <img src="LOGO.png" className="h-full" alt="" />
+        {/* <h1 className='text-white flex px-12 text-4xl font-bold'>Runnify</h1> */}
       </header>
       <main>
-          <Outlet />
+        <Outlet />
       </main>
-      <footer>
-      </footer>
+      <footer></footer>
     </div>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
