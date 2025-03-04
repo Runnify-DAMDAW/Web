2abc37bc8e8cc1b1a73731d0ed5a3b44adc451b3
daviiidleon <daviiidleongutierrez@gmail.com>
2025-03-04 18:42:20 +0100

    vista error 1

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-gray-800 text-2xl font-bold">
+      <p>⚠️ Error: Página no encontrada</p>
+      <Link to="/" className="mt-4 px-4 py-2 bg-gray-800 text-white rounded-lg shadow hover:bg-gray-900 transition">
+        Volver a Inicio
+      </Link>
+    </div>
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
