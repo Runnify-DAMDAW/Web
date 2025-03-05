37377a27e64f67f899110d5c6f6edb31754d391f
adri1211 <adrian.ariza1211@gmail.com>
2025-03-05 17:28:22 +0100

    modificacion fetch home

---
+++ src/contexts/CarreraContext.jsx
@@ -0,0 +1,52 @@
+import { createContext, useContext, useEffect, useState } from "react";
+const API_URL = import.meta.env.VITE_CARRERAS_URL;
+
+const CarreraContext = createContext();
+
+
+export const useCarreras = () => {
+    const contex = useContext(CarreraContext);
+    if (!contex) {
+        throw new Error('useCarreras must be used within an CarreraProvider');
+    }
+
+    return contex;
+
+};
+
+
+export const CarreraProvider = ({children}) =>{
+
+    const [carreras, setCarreras] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+		loadData();
+	}, []);
+
+    const loadData = async () => {
+		try {
+			const response = await fetch(`${API_URL}`);
+			if (!response.ok) {
+				console.error('error en', response);
+                setError(error);
+			} 
+            const data = await response.json();
+            setCarreras(data);
+		} catch (error) {
+			console.error(error);
+            setError(error);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+    return (
+        <CarreraContext.Provider value={{ carreras, loading, error }}>
+            {children}
+        </CarreraContext.Provider>
+    );
+
+
+}
--- src/pages/CarreraDetails.jsx
+++ src/pages/CarreraDetails.jsx
@@ -16,40 +16,14 @@ const carrera = {
 	imagen: ""
 }
 
-const CarreraDetails = () => {
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+
+
+  const CarreraDetails = () => {
   const location = useLocation();
   const navigate = useNavigate();
   const carrera = location.state?.carrera; 
-=======
-	return (
-		<div className='h-screen px-10 py-5 flex [&>div]:mx-6 [&>div]:w-1/2'>
-			<div className='bg-red-5000'>
-				<h1 className='font-bold text-6xl'>{carrera.name}</h1>
-				<p className=''>
-					{carrera.description}
-				</p>
-				<p className='my-6'>
-					<span className={`text-lg px-2 py-1 rounded-xl border-4 ${carrera.status === "Abierta"? "bg-green-500 border-green-700" : "bg-red-500 border-red-700"}`}>
-						{carrera.status}
-					</span>
-				</p>
-			</div>
-			<div className='bg-green-5000 flex justify-between'>
-				<p className='text-xl'>{carrera.date}</p>
-				<p className='text-xl'>{carrera.location}</p>
-				<p className='ml-6 text-xl'>{carrera.distance_km} km</p>
-			</div>
-		</div>
-	)
-}
->>>>>>> origin/Arturo
-=======
-  const location = useLocation();
-  const navigate = useNavigate();
-  const carrera = location.state?.carrera; 
->>>>>>> origin/Arturo
+
 
   if (!carrera) {
     return <p className="text-red-500">Carrera no encontrada 🖕</p>;
