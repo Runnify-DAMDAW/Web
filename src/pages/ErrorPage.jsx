8afd51c693418e6c794a498b9a9f31f91b8408a2
David León <114946370+daviiidleon@users.noreply.github.com>
2025-03-05 19:35:05 +0100

    nuevo commit

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
-  const navigate = useNavigate();
-
-  // Función que maneja la navegación al Home
-  const handleGoHome = () => {
-    navigate('/home');  // Debería redirigir al Homee
-  };
-
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-gray-800 text-2xl font-bold">
-      <img src="error.png" alt="Error" className="w-64 h-auto mb-4" />
+      <img src="error.png" alt="Error" className="w-96 h-auto mb-4" />
       <p>Error: Página no encontrada</p>
-      <button 
-        onClick={handleGoHome} 
-        className="mt-4 px-4 py-2 bg-gray-800 text-white rounded-lg shadow hover:bg-gray-900 transition"
-      >
+      <Link to="/home" className="mt-4 px-4 py-2 bg-gray-800 text-white rounded-lg shadow hover:bg-gray-900 transition">
         Volver a Inicio
-      </button>
+      </Link>
     </div>
   );
 };
