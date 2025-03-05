3b23f865b9185e1a1002ee20a643222f6b20af85
RociooGaviraa <rociogaviralucena@gmail.com>
2025-03-05 16:48:33 +0100

    correcion

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -2,18 +2,26 @@ import React from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 
 const RootLayout = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
     <div className="bg-[#909590]">
       <header className="sticky top-0 w-full h-36 px-16 flex items-center bg-gradient-to-r from-[#537a5a] to-[#2c302e]">
-        <img src="LOGO.png" className="h-full" alt="" />
+        <img
+          src="LOGO.png"
+          className="h-full cursor-pointer"
+          alt="Logo"
+          onClick={() => navigate("/home")}
+        />
         {/* <h1 className='text-white flex px-12 text-4xl font-bold'>Runnify</h1> */}
       </header>
       <main>
         <Outlet />
       </main>
       <footer className="text-white w-full h-20 px-16 flex items-center bg-gradient-to-r from-[#2c302e] to-[#87ad65]">
-        <p className='flex px-12 text-sm'> 2025 Runnify. All rights reserved.</p>
+        <p className="flex px-12 text-sm">
+          {" "}
+          2025 Runnify. All rights reserved.
+        </p>
         <ul className="flex [&>li]:font-bold [&>li]:mx-8">
           <li>Adrián</li>
           <li>Arturo García</li>
