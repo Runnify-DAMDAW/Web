2b7a3bc6c01ae4572cd75375194da00a1a0073e5
David León <114946370+daviiidleon@users.noreply.github.com>
2025-03-05 21:06:48 +0100

    nuevo commit

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -41,14 +41,8 @@ const RootLayout = () => {
 
   return (
     <div className="bg-[#909590]">
-      <header className="sticky top-0 w-full h-36 px-16 flex items-center bg-gradient-to-r from-[#537a5a] to-[#2c302e]">
-        <img
-          src="LOGO.png"
-          className="h-full cursor-pointer"
-          alt="Logo"
-          onClick={() => navigate("/home")}
-        />
-        {/* <h1 className='text-white flex px-12 text-4xl font-bold'>Runnify</h1> */}
+
+
       <header className="z-50 sticky top-0 w-full h-36 px-16 flex items-center bg-gradient-to-r from-[#87ad65] to-[#2c302e]">
         <button onClick={() => setExtended((prev) => !prev)} className="cursor-pointer">
           <MenuSharpIcon title="Fecha" style={{ fontSize: 35, color: 'white' }} />
