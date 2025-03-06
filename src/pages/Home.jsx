5f54abcc2a8af01e6d57d13e1f7cb77822da3f46
ArturoGarGarcia2 <arturogargarcia07@gmail.com>
2025-03-06 19:22:51 +0100

    Añadido filtro status

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,13 @@ import Spinner from '../components/Spinner';
 import FilterCategoria from '../components/FilterCategoria';
 import Filter from '../components/Filter';
 import { useCarreras } from '../contexts/CarreraContext';
+import FilterStatus from '../components/FilterStatus';
 
 const Home = () => {
   const { carreras, loading } = useCarreras();
   const [searchTerm, setSearchTerm] = useState('');
   const [searchTermCategoria, setSearchTermCategoria] = useState('');
+  const [searchTermStatus, setSearchTermStatus] = useState('');
   const [filteredCarreras, setFilteredCarreras] = useState([]);
 
   useEffect(() => {
@@ -21,11 +23,14 @@ const Home = () => {
       )
       .filter(carrera =>
         searchTermCategoria === "" || carrera.category.toLowerCase() === searchTermCategoria.toLowerCase()
+      )
+      .filter(carrera =>
+        searchTermStatus === "" || carrera.status.toLowerCase() === searchTermStatus.toLowerCase()
       );
 
       setFilteredCarreras(filtered);
     }
-  }, [searchTerm, searchTermCategoria, carreras]);
+  }, [searchTerm, searchTermCategoria, searchTermStatus, carreras]);
   
   if (loading) {
     return <Spinner />;
@@ -36,6 +41,7 @@ const Home = () => {
       <div className='flex justify-center mb-4 mt-2'>
         <Filter searchTerm={searchTerm} onSearchChange={setSearchTerm} />
         <FilterCategoria searchTerm={searchTermCategoria} onSearchChange={setSearchTermCategoria} />
+        <FilterStatus searchTerm={searchTermStatus} onSearchChange={setSearchTermStatus} />
       </div>
       <div className='flex items-center self-center px-24'>
         <div className='flex flex-wrap justify-start items-start my-8 w-full'>
