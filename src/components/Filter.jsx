067821b589852d26ca0222bd2a7245b7cbe8abd3
RociooGaviraa <rociogaviralucena@gmail.com>
2025-03-07 20:39:26 +0100

    Filtro inscripcion

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 const Filter = ({ searchTerm, onSearchChange }) => {
   return (
-    <div className="relative inline-block text-left">
-      <input
-        type="text"
-        value={searchTerm}
-        onChange={(e) => onSearchChange(e.target.value)}
-        placeholder="Buscar por nombre..."
-        className="bg-gray-800 text-white px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder-gray-400"
-      />
+    <div className="flex items-center gap-4">
+      <div className="relative inline-block text-left">
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => onSearchChange(e.target.value)}
+          placeholder="Buscar por nombre..."
+          className="bg-gray-800 text-white px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 placeholder-gray-400"
+        />
+      </div>
     </div>
   );
 };
